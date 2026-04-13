00ab45309e6db24795e9d4bd628d2a3af046ba98
Andy-Z0z <viktor35146643@gmail.com>
2026-04-13 19:31:56 +0500

    Создан файл Core.h для модуля Quant Engine

---
+++ QuantEngine/Core/Core.h
@@ -0,0 +1,8 @@
+#pragma once
+// Quant Engine v1000.r.f1 — Core Module Header
+#include "Logging/QLog.h"
+#include "Memory/QAllocator.h"
+#include "JobSystem/QJobSystem.h"
+#include "Profiling/QProfiler.h"
+
+// Дополнительные ядровые include по необходимости
